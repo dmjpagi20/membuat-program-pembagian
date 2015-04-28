78a228a99dee0492b080a22eee1f417729d32455
SBC4 <SBC4@SBC03>
2015-04-28 12:44:36 +0700

    pembagian

---
+++ src/pembagian/Pembagian.java
@@ -0,0 +1,19 @@
+
+package pembagian;
+
+
+public class Pembagian {
+
+    
+    public static void main(String[] args) {
+int x = 20;
+int y = 4;
+int s = x/y;
+        System.out.println("nilai= " + x);
+        System.out.println("nilai= " + y);
+        System.out.println("nilai= " + s);
+    }
+}
+ 
+    
+
